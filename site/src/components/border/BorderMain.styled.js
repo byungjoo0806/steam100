93d4abc117f05d51c1373bc3ff2c5a5309155463
byungjoo0806 <andybyungjoopark@gmail.com>
2023-08-17 14:35:59 +0900

    0817 react app build for deploy

--- site/src/components/border/BorderMain.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const BorderBox = styled.div`
-    width: 500px;
-    height: 500px;
-    border: 1px solid;
-    background-color: whitesmoke;
-`
--- site/src/img/index.js
+++
@@ -1,5 +0,0 @@
-import testImg1 from './bicycle.jpg'
-import adminImg from './admin.png'
-import loadingGIF from './loading.gif'
-
-export { testImg1, adminImg, loadingGIF }
