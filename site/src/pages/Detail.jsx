692b36231b713db28ce41bd00df5add592ad779f
byungjoo0806 <andybyungjoopark@gmail.com>
2023-08-17 17:36:19 +0900

    0817 detail page rendering fixing

--- site/src/pages/Detail.jsx
+++ site/src/pages/Detail.jsx
@@ -84,7 +84,7 @@ const Detail = () => {
                     about this game : <br/> {finalRenderingDescription}
                 </div>
                 <div style={{width : "100%", height : "auto", borderBottom : "1px solid"}}>
-                    Hello
+                    Hello HI
                 </div>
                 <div style={{width : "100%", height : "auto"}}>
 
