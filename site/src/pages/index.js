c64c19e108a78b3b8e29dbf3058387ba6ef25a50
byungjoo0806 <andybyungjoopark@gmail.com>
2023-08-21 17:52:09 +0900

    0821 git merge error fixing

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,11 +1,7 @@
 import { Border } from './Border'
 import { Login } from './Login'
 import { Main } from './Main';
-<<<<<<< HEAD
-import Signup from './Signup';
-=======
 import SignUp from './Signup';
->>>>>>> jisub
 import Mypage from './Mypage';
 import AdminPage from './AdminPage'
 import Detail from './Detail'
@@ -13,8 +9,4 @@ import Top10 from './Top10';
 import BorderInsert from './BorderInsert';
 import BorderDetail from './BorderDetail';
 
-<<<<<<< HEAD
-export { Border, Login, Main, Signup, Mypage, AdminPage, Detail, Top10 };
-=======
 export { Border, Login, Main, SignUp, Mypage, AdminPage, Detail, Top10, BorderInsert, BorderDetail };
->>>>>>> jisub
